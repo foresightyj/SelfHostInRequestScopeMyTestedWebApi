448bf78fe1f832ee69f19ec9c59008319ceb39fa
foresightyj <imyuanjian@gmail.com>
2016-04-19 22:40:25 +0800

    Still works

--- SelfHostInRequestScopeMyTestedWebApi.Tests/Controllers/ValuesControllerTest.cs
+++ SelfHostInRequestScopeMyTestedWebApi.Tests/Controllers/ValuesControllerTest.cs
@@ -1,14 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyTested.WebApi;
-using SelfHostInRequestScopeMyTestedWebApi;
-using SelfHostInRequestScopeMyTestedWebApi.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Web.Http;
 
 namespace SelfHostInRequestScopeMyTestedWebApi.Tests.Controllers
 {
